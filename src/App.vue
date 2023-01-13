0356d6f1ae59a584f3431ef6040985188ce7f7e1
myonaingwinn <myonaingwin.gic@gmail.com>
2023-01-13 15:41:27 +0630

    computed values

--- src/App.vue
+++ src/App.vue
@@ -30,10 +30,13 @@ export default defineComponent({
     return { ...toRefs(state) }; */
 
     const jobs = ref<Job[]>([
-      { id: "2kjdk", title: "Farmer", location: "Myanmar", salary: 230000 },
-      { id: "jasdk", title: "Farmer", location: "India", salary: 230000 },
-      { id: "3sadk", title: "Farmer", location: "Philp", salary: 220000 },
-      { id: "2bjdl", title: "Farmer", location: "Singapore", salary: 250000 },
+      { id: "2kjdk", title: "MC Enginner", location: "Myanmar", salary: 2350000 },
+      { id: "jasdk", title: "Farmer", location: "India", salary: 2300000 },
+      { id: "3sadk", title: "DevOps", location: "Philp", salary: 2200000 },
+      { id: "2bjdl", title: "Cloud Engineer", location: "Singapore", salary: 3400000 },
+      { id: "2bjdl", title: "Infrastructure Engineer", location: "North America", salary: 2100000 },
+      { id: "2bjdl", title: "Network Engineer", location: "China", salary: 5000000 },
+      { id: "2bjdl", title: "Security Administrator", location: "Thailand", salary: 3000000 },
     ]);
 
     const orderTerm = ref<OrderTerm>("title");
--- src/components/JobList.vue
+++ src/components/JobList.vue
@@ -3,7 +3,7 @@
     <h1>Job List</h1>
     <i>Ordered by {{ order }}</i>
     <ul>
-      <li v-for="job in jobs" :key="job.id">
+      <li v-for="job in orderedJobs" :key="job.id">
         <h2>{{ job.title }} in {{ job.location }}</h2>
         <div class="salary">
           <p>{{ job.salary }} kyats</p>
@@ -22,7 +22,7 @@
 <script lang="ts">
 import Job from "types/Job";
 import OrderTerm from "types/OrderTerm";
-import { defineComponent, PropType } from "vue";
+import { computed, defineComponent, PropType } from "vue";
 
 export default defineComponent({
   //   setup() {},
@@ -35,6 +35,17 @@ export default defineComponent({
       required: true,
       type: String as PropType<OrderTerm>,
     },
+  },
+  setup(props) {
+    const orderedJobs = computed(() => {
+      return [...props.jobs].sort((a: Job, b: Job) => {
+        if (a[props.order] === "salary" && b[props.order] === "salary")
+          return a[props.order] < b[props.order] ? -1 : 1;
+        else return a[props.order] > b[props.order] ? 1 : -1;
+      });
+    });
+
+    return { orderedJobs };
   },
 });
 </script>
